b5cb6d95086c567af11aa9af5e8b0e9a6304317d
jaebe <putgreenapple@gmail.com>
2024-03-17 00:25:08 +0900

    .env 파일을 사용하도록 수정

--- domain/user/user_router.py
+++ domain/user/user_router.py
@@ -10,9 +10,11 @@ from starlette import status
 from database import get_db
 from domain.user import user_crud, user_schema
 from domain.user.user_crud import pwd_context
+from starlette.config import Config
 
-ACCESS_TOKEN_EXPIRE_MINUTES = 60 * 24
-SECRET_KEY = "4abd108a3d35ad0c90e3a691deeb2efd15798453c69110d8d2deb123e1f21027"
+config = Config('.env')
+ACCESS_TOKEN_EXPIRE_MINUTES = int(config('ACCESS_TOKEN_EXPIRE_MINUTES'))
+SECRET_KEY = config('SECRET_KEY')
 ALGORITHM = "HS256"
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/user/login")
 
